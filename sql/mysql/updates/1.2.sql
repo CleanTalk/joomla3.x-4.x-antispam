3cfa08da84f6792ecaef13fe3d2f00e8fc6450a6
davydovct <davydovct@cleantalk.org>
2019-12-11 16:18:02 +0500

    remove 1.2 update

--- sql/mysql/updates/1.2.sql
+++
@@ -1,8 +0,0 @@
-# Dummy SQL file to set schema version to 1.1 so next update will work
-CREATE TABLE IF NOT EXISTS `#__cleantalk_sessions` (
-    `id` varchar(64) NOT NULL,
-    `name` varchar(40) NOT NULL,
-    `value` text NULL DEFAULT NULL,
-    `last_update` datetime NULL DEFAULT NULL,
-    PRIMARY KEY (`name`(40), `id`(64))
-);
