4eac445a3077b96ac722e8ddf98d9a459aad980f
Glomberg <bazz@bk.ru>
2023-06-12 18:28:09 +0300

    Upd. Users checker. System files updated - schemas, manifest.

--- sql/sqlsrv/uninstall.sqlsrv.utf8.sql
+++ sql/sqlsrv/uninstall.sqlsrv.utf8.sql
@@ -2,3 +2,4 @@ DROP TABLE IF EXISTS `#__cleantalk_sfw`;
 DROP TABLE IF EXISTS `#__cleantalk_sfw_logs`;
 DROP TABLE IF EXISTS `#__cleantalk_sessions`;
 DROP TABLE IF EXISTS `#__cleantalk_ua_bl`;
+DROP TABLE IF EXISTS `#__cleantalk_usermeta`;
