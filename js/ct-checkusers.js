ea94cb262c475acdb96f8c16b7a53b838ca98944
Glomberg <bazz@bk.ru>
2024-08-06 14:45:41 +0300

    Fix. Users checker. Control buttons listeners fixed.

--- js/ct-checkusers.js
+++ js/ct-checkusers.js
@@ -163,6 +163,7 @@ window.apbct = window.apbct || {};
             usersChecker.loadScanResults(1).then(
                 content => {
                     usersChecker.loadScanResultsSuccess(content);
+                    usersChecker.setListeners();
                 },
                 error  => {
                     usersChecker.loadScanResultsError(error);
